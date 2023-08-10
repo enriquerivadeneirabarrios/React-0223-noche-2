25a748c6e6ac7dfe1e729bde31adb618ae8e4666
nahuedt <nahuelbarrientos.dt@gmail.com>
2023-08-10 13:00:22 -0300

    se creo una navbar con react bootstrap.
    (falta diseño)

---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Freshness</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">Productos</Nav.Link>
+            <Nav.Link href="#link">Carrito</Nav.Link>
+            <NavDropdown title="Mas" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Comentarios</NavDropdown.Item>
+              
+              <NavDropdown.Item href="#action/3.2">About Us</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">
+               Redes Sociales
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
+ 
