9614cb0bca3bd1029cd4b269b470e486ed107b10
nahuedt <nahuelbarrientos.dt@gmail.com>
2023-08-11 11:07:31 -0300

    se agrego carpeta imagen y logo

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,11 +3,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Bubblegum_Sans } from 'next/font/google';
 
 function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
+      <img
+          className="freshness"
+          src= "./images/freshness.jpg"
+          alt="freshness"
+          width="70px"
+      />
         <Navbar.Brand href="#home">Freshness</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
