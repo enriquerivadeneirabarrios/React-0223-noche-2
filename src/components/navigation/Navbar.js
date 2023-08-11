5da96386116d4c7d81ac75d73853c4bc887a3b5b
nahuedt <nahuelbarrientos.dt@gmail.com>
2023-08-11 12:45:44 -0300

    se agrego icono de carrito

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -7,20 +7,21 @@ import { Bubblegum_Sans } from 'next/font/google';
 
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-light">
       <Container>
-      <img
-          className="freshness"
-          src= "./images/freshness.jpg"
-          alt="freshness"
-          width="70px"
-      />
-        <Navbar.Brand href="#home">Freshness</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            className="freshness"
+            src= "./images/freshness.jpg"
+            width="70px"
+            alt="freshness"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
             <Nav.Link href="#home">Productos</Nav.Link>
-            <Nav.Link href="#link">Carrito</Nav.Link>
+            
             <NavDropdown title="Mas" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Comentarios</NavDropdown.Item>
               
@@ -32,6 +33,15 @@ function BasicExample() {
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+        
+        <Nav.Link href="#link">
+          <img
+            className="carrito"
+            src='./images/logo-tienda32pxblack.png'
+            width="32px"
+            alt="cart"
+          />
+        </Nav.Link>
       </Container>
     </Navbar>
   );
