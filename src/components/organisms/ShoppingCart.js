b82a95c5b202ae4757db1bca27e3597300a167ab
enriquerivadeneirabarrios <e.rivadeneira25@gmail.com>
2023-08-16 17:48:26 -0400

    probando agregar el accordeon

--- src/components/organisms/ShoppingCart.js
+++ src/components/organisms/ShoppingCart.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer, useId } from 'react'
+
 import { TYPES } from '../../actions/ShoppingActions.js'
 import CartItems from '../molecules/CartItems'
 import {shoppingReducer} from '../../reducer/ShoppingReducer'
@@ -6,6 +7,21 @@ import {shoppingInitialState} from '../../reducer/shoppingReducer'
 import Button from '../atoms/Button'
 import Card from '../atoms/Card'
 import styled from 'styled-components'
+
+
+function CartIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' strokeWidth='1' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0' />
+      <path d='M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0' />
+      <path d='M17 17h-11v-14h-2' />
+      <path d='M6 5l14 1l-1 7h-13' />
+    </svg>
+  )
+}
+
+const cartCheckboxId = 15;
 
 
 
@@ -40,8 +56,12 @@ const clearCart = () => {
 
     return (
     <>
+          <label className='cart-button' htmlFor={cartCheckboxId}>
+        <CartIcon />
+      </label>
+      <input id={cartCheckboxId} type='checkbox' hidden />
 
-         <Carrito>
+         <Carrito className="cart">
         <Title>Carrito De Compras</Title>
           <CartCards>
             {cart.map((item, i) =>(
@@ -69,6 +89,43 @@ const clearCart = () => {
           <Card data={product} addToCart={addToCart} />
         ))}         
       </Product>
+
+      <style jsx>{`
+      .cart {
+        display:none
+      }
+
+      .cart-button {
+        align-items: center;
+        background: #09f;
+        border-radius: 9999px;
+        cursor: pointer;
+        display: flex;
+        height: 32px;
+        justify-content: center;
+        padding: 4px;
+        position: absolute;
+        right: 8px;
+        top: 8px;
+        transition: all .3s ease;
+        width: 32px;
+        z-index: 9999;
+      }
+      
+      .cart-button:hover {
+        scale: 1.1;
+      }
+      
+      .cart-button ~ input:checked ~ .cart {
+        height: 100%;
+        display: grid;
+      }
+
+
+      
+      `}
+
+      </style>
       
     </>
   );
@@ -97,7 +154,7 @@ padding-top: 10px;
 padding-bottom: 50px;
 background-color: white;
 
-display:grid;
+
 grid-template-columns: repeat(auto-fit,minmax(150px,1vh));
 grid-template-rows:repeat(auto-fit,minmax(10vh,auto));
 grid gap: 2px;
