7fa8a3218934fe736f16f94e9e63d512661f81eb
Paula Velásquez <pvelaskez@gmail.com>
2023-08-18 17:56:39 -0400

    Footer versión antigua

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
-import styled from 'styled-components'
+
 
 class Footer extends Component {
   render() {
     const current_year = new Date().getFullYear();
     return (
-        <Container>
-            
+        <div id="section_footer">
+
             <div className='text-center py-4'>
             <Link href="https://campus.academianumen.com/" target="_blank" className='btn btn-link'>
                 Privacy
@@ -20,45 +20,14 @@ class Footer extends Component {
             <Link href="https://campus.academianumen.com/" target="_blank" className='btn btn-link'>
                 Contact
             </Link>
-            <Paragraph>Copyright © {current_year}, ACADEMIA NUMEN. All Rights Reserved.</Paragraph>
+            <p>Copyright © {current_year}, ACADEMIA NUMEN. All Rights Reserved.</p>
             </div>
-      </Container>
+      </div>
     );
   }
 }
 
 export default Footer;
-
-
-
-const Container = styled.div`
-display: flex;
-flex-direction: column;
-height: 20vh;
-margin-top:  20vh;
-
-width: 100%;
-background:  #76b631;
-block-size: auto;
-color: #fff;
-text-decoration: none;
-margin-top: auto;
-
-
-
-hover {
-  text-decoration: underline;
-  color: #519607;}
-`
-
-const Paragraph = styled.p`
-color: #fff;
-color: #fff;
-font-size: .7rem;
-text-decoration: none;
-`
-
-
 
 
 
